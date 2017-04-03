9e9ab5358a555a825393956b52342c6865c27d3e
MAZI_ <dnl.mazur@gmail.com>
2017-04-03 19:17:40 +0200

    Zapis danych + klienci
    Obsługa zapisywania z formularza zamównienia posiłku
    Zaczytanie danych do listy klientów i przygotowanie wstępnego
    zarządzania tymi danymi

--- admin/services/get_client_data.php
+++ admin/services/get_client_data.php
@@ -1,19 +1,17 @@
 <?php
-$date = isset($_GET['date']) ? $_GET['date'] : ''; 
 require_once("../../resources/php/classes/MySqlDBConnection.class.php");
 $db_handle = new MySqlDBConnection();
 $result["total"] = 0;
 $result["success"] = false;
-$queryStr = sprintf('SELECT d.id id, d.name name, SUM(o.AMOUNT) count FROM %1$s.dinner d LEFT JOIN %1$s.order o ON d.ID = o.DINNER_ID WHERE d.DATE = ? GROUP BY d.ID, d.NAME', $db_handle->dbName);
-$params = array("s", $date);
-$rs = $db_handle->runPreparedQuery($queryStr, $params);
+$queryStr = sprintf('SELECT * FROM %1$s.client', $db_handle->dbName);
+$rs = $db_handle->runQuery($queryStr);
 
 if ($rs != null) {	
 	$result['total'] = sizeof($rs);	
 	$result["success"] = true;
 	$result["rows"] = $rs;
 	$result["success"] = true;
-	$result["msg"] = 'Products successfuly requested.';
+	$result["msg"] = 'Klienci pobrani';
 } else {
 	$result["msg"] = 'Sorry, unable to retrieve the rows from the database!';
 }
--- clients/MAZI_/services/get_orders_today.php
+++ clients/MAZI_/services/get_orders_today.php
@@ -15,16 +15,12 @@ $resultset['success'] = false;
 $resultset['data'] = [];
 
 if ($result != null) {
+	$resultset['success'] = true;
+	
 	foreach ($result as $row) {
 		$query = sprintf('SELECT O.* FROM %1$s.ORDER O LEFT JOIN %1$s.CLIENT C ON O.CLIENT_ID = C.ID WHERE O.DINNER_ID = ? AND C.NAME = ? AND O.DATE BETWEEN ? AND ?', $db_handle->dbName);
 		$params = array("isss", $row['ID'], $clientName, $dateToday, $endDateToday);
 		$result2 = $db_handle->runPreparedQuery($query, $params);
-		
-		if ($result2 == null) {
-			continue;
-		}
-		
-		$resultset['success'] = true;
 		$objresult = (object) [
 			'dinner' => $row,
 			'orders' => $result2
